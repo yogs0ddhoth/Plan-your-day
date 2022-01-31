c5e9879d02eb07678299137ea83b24414973469f
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-01-31 13:45:08 -0500

    rendered timeblocks with jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,43 @@
 // declare global variables
+const $plannerDiv = $('#planner');
 
+// jquery method of rendering timeblocks:
+
+//  timeblock div
+let $timeBlockDiv = $('<div>');
+$timeBlockDiv.addClass('input-group');
+
+let $timeDiv = $('<div>');
+$timeDiv.addClass('input-group-prepend');
+$timeBlockDiv.append($timeDiv);
+
+let $timeSpan = $('<span>');
+$timeSpan.addClass('input-group-text');
+  // render hour in $timeSpan
+  $timeSpan.text();
+$timeDiv.append($timeSpan);
+
+// chose either textarea or text input and add corresponding bootstrap classes
+let $eventInput = $('<textarea>');
+$eventInput.addClass('form-control');
+$eventInput.attr('aria-label', 'With textarea' );
+$eventInput.insertAfter($timeDiv);
+
+// let $eventInput = $();
+// $eventInput.addClass();
+// $eventInput.insertAfter($timeDiv);
+
+let $saveDiv = $('<div>');
+$saveDiv.addClass('input-group-append');
+$saveDiv.insertAfter($eventInput);
+
+let $saveBtn = $('<button>');
+$saveBtn.addClass('btn btn-outline-secondary');
+$saveBtn.attr('type', 'button' );
+$saveBtn.text("save");
+$saveDiv.append($saveBtn);
+
+$plannerDiv.append($timeBlockDiv);
 /*
   timeblock functionality:
   * color based on time - moment
