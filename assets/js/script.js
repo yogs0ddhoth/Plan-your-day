519a8f3b27d1eef933067ca8e2d5d366c673d3b0
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-01-31 17:57:19 -0500

    created timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,75 @@
+// render local date in html
+$('#planner').text(moment().format("[Today is] dddd, MMMM Do YYYY"));
+
 // declare global variables
 const $plannerDiv = $('#planner');
+let now;
+let currentHour = moment().format('hA');
 
-// jquery method of rendering timeblocks:
+// function to render 9 timeblocks:
+function renderTimeBlocks() {
+  for (let hour = 9; hour <= 17; hour++) {
+    // render timeblocks
+    let $timeBlockDiv = $('<div>');
+    $timeBlockDiv.addClass('input-group ');
 
-//  timeblock div
-let $timeBlockDiv = $('<div>');
-$timeBlockDiv.addClass('input-group');
+    let $timeDiv = $('<div>');
+    $timeDiv.addClass('input-group-prepend col-2');
+    $timeBlockDiv.append($timeDiv);
 
-let $timeDiv = $('<div>');
-$timeDiv.addClass('input-group-prepend');
-$timeBlockDiv.append($timeDiv);
+    let $timeSpan = $('<span>');
+    $timeSpan.addClass('input-group-text col-12');
+    let time;
+    let ampm;
+    if (hour > 12) {
+      time = hour - 12;
+      ampm = "pm";
+    } else {
+      time = hour;
+      ampm = "am";
+    }
+    $timeSpan.text(time + ampm);
+    $timeDiv.append($timeSpan);
+    
+    // chose either textarea or text input and add corresponding bootstrap classes
+    
+    let $eventInput = $('<textarea>');
+    $eventInput.addClass('form-control col-9');
+    $eventInput.attr('aria-label', 'With textarea' );
+    $eventInput.insertAfter($timeDiv);
 
-let $timeSpan = $('<span>');
-$timeSpan.addClass('input-group-text');
-  // render hour in $timeSpan
-  $timeSpan.text();
-$timeDiv.append($timeSpan);
+    // let $eventInput = $('<input>');
+    // $eventInput.attr({
+    //   type:'text',
+    // });
+    // $eventInput.addClass('form-control');
+    
+    $eventInput.insertAfter($timeDiv);
 
-// chose either textarea or text input and add corresponding bootstrap classes
-let $eventInput = $('<textarea>');
-$eventInput.addClass('form-control');
-$eventInput.attr('aria-label', 'With textarea' );
-$eventInput.insertAfter($timeDiv);
+    let $saveDiv = $('<div>');
+    $saveDiv.addClass('input-group-append col-1');
+    $saveDiv.insertAfter($eventInput);
 
-// let $eventInput = $();
-// $eventInput.addClass();
-// $eventInput.insertAfter($timeDiv);
+    let $saveBtn = $('<button>');
+    $saveBtn.addClass('btn btn-outline-secondary');
+    $saveBtn.attr('type', 'button' );
+    $saveBtn.text("save");
+    $saveDiv.append($saveBtn);
 
-let $saveDiv = $('<div>');
-$saveDiv.addClass('input-group-append');
-$saveDiv.insertAfter($eventInput);
+    $plannerDiv.append($timeBlockDiv);
+  }
+  // timeblock functionality:
+  // * color based on time - use moment
+}
 
-let $saveBtn = $('<button>');
-$saveBtn.addClass('btn btn-outline-secondary');
-$saveBtn.attr('type', 'button' );
-$saveBtn.text("save");
-$saveDiv.append($saveBtn);
+renderTimeBlocks()
 
-$plannerDiv.append($timeBlockDiv);
+
+
 /*
+  TODO:
+
   timeblock functionality:
-  * color based on time - moment
+  * color based on time - use moment
   * function to save input to local storage
 */
